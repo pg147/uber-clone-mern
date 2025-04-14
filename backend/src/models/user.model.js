4c39873b47b851f4fd300d0c9a227662578b9336
pg147 <xyzprathamesh147@gmail.com>
2025-04-14 18:03:45 +0530

    added: models - user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: [2, 'First Name must be at least 2 characters long!']
+        },
+        lastName: {
+            type: String,
+            required: true,
+            minLength: [3, 'Last Name must be at least 3 characters long!']
+        }
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    socketId: {
+        type: String
+    }
+});
+
+userSchema.methods.generateAuthToken = function () {
+    return jwt.sign({_id: this._id}, process.env.JWT_SECRET,);
+}
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+export const User = mongoose.model('user', userSchema);
+
