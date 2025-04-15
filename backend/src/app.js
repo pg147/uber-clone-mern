e569fa05519ba21a4dd83d11742783bdf383d684
pg147 <xyzprathamesh147@gmail.com>
2025-04-15 21:12:57 +0530

    update: added json & url middlewares

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import userRouter from './routes/user.route.js';
 
 dotenv.config(); // initialized dotenv for env variables
 
 const app = express();  // initialized express app
 
+app.use(express.json());  // middleware to parse data in json
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());  // middleware for cross-origin-resource-sharing
-
-app.get('/', (req, res) => {
-    res.send('Hello from app!');
-});
+app.use('/api/v1/users', userRouter);
 
 export { app };
