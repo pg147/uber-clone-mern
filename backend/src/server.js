b00c656c4145a71f1bc3b96cfbfc1070a218cbd4
pg147 <xyzprathamesh147@gmail.com>
2025-04-15 21:13:16 +0530

    update: minor changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import http from 'http';
 import {app} from "./app.js";
-import connectDB from "../db/db.js";
+import connectDB from "./db/db.js";
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
