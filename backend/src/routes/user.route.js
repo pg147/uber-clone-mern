fb557b90d873ebe31f7d52349dce9f1156cc656a
pg147 <xyzprathamesh147@gmail.com>
2025-04-16 18:44:17 +0530

    update: getUserProfile route added

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { validateUserDetails } from "../middlewares/validations.middleware.js";
-import {login, signup} from "../controllers/user.controller.js";
+import { getUserProfile, login, signup } from "../controllers/user.controller.js";
+import { authUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post('/create', validateUserDetails, signup);
 router.post('/login', validateUserDetails, login);
+router.get('/profile', authUser, getUserProfile);
 
 export default router;
