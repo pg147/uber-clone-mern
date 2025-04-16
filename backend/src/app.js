1544d345bf9b3f4426117abcf1e42407cae2f62a
pg147 <xyzprathamesh147@gmail.com>
2025-04-16 18:44:37 +0530

    update: cookie-parser middleware integrated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
+// Dependencies
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+// Routes
 import userRouter from './routes/user.route.js';
 
 dotenv.config(); // initialized dotenv for env variables
@@ -9,6 +13,7 @@ const app = express();  // initialized express app
 
 app.use(express.json());  // middleware to parse data in json
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());  // middleware to parse cookies
 app.use(cors());  // middleware for cross-origin-resource-sharing
 app.use('/api/v1/users', userRouter);
 
