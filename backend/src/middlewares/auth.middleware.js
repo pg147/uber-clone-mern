3b5a0c1c010cdb0a29c98c89973759c872ace581
pg147 <xyzprathamesh147@gmail.com>
2025-04-19 01:15:13 +0530

    update: authCaptain logic

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import { ResponseError } from "../libs/utils.js";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import blacklistTokenModel from "../models/blacklistToken.model.js";
+import captainModel from "../models/captain.model.js";
 
 export async function authUser(req, res, next) {
     // Finding token either through cookies or headers
@@ -24,5 +25,32 @@ export async function authUser(req, res, next) {
     } catch (error) {
         console.log(error.message);
         return ResponseError(res, 500, "Internal server error!");
+    }
+}
+
+export async function authCaptain(req, res, next) {
+    // Finding token either through cookies or headers
+    // In headers, authorization consists of 'bearer' followed by JWT String, hence splitting
+    const token = req.cookies?.token || req.headers?.authorization?.token;
+
+    // If token doesn't exist
+    if (!token) return ResponseError(res, 401, 'Unauthorized access!');
+
+    // Checking if the captain's token is blacklisted
+    const isBlacklisted = await blacklistTokenModel.findOne(token);
+
+    // If the token is blacklisted -> send error
+    if (isBlacklisted) return ResponseError(res, 400, 'Unauthorized access!');
+
+    try {
+        const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decodedToken._id);
+
+        req.captain = captain;
+
+        return next();
+    } catch (error) {
+        console.log(error.message);
+        return ResponseError(res, 500, 'Internal server error!');
     }
 }
