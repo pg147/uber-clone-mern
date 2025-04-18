56eed3044910f09b2779daccd07e0d4d530fd514
pg147 <xyzprathamesh147@gmail.com>
2025-04-19 01:15:42 +0530

    update: captain routes & comments at user routes

--- backend/src/routes/captain.route.js
+++ backend/src/routes/captain.route.js
@@ -1,10 +1,21 @@
 import express from "express";
 import { validateCaptainDetails } from "../middlewares/validations.middleware.js";
-import { loginCaptain, registerCaptain } from "../controllers/captain.controller.js";
+import { getCaptainProfile, loginCaptain, logoutCaptain, registerCaptain } from "../controllers/captain.controller.js";
+import { authCaptain } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+// Register Captain
 router.post('/create', validateCaptainDetails, registerCaptain);
+
+// Login Captain
 router.post('/login', validateCaptainDetails, loginCaptain);
+
+// Logout Captain
+router.get('/logout', authCaptain, logoutCaptain);
+
+// Fetch Captain Profile
+router.get('/profile', authCaptain, getCaptainProfile);
+
 
 export default router;
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -5,8 +5,13 @@ import { authUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+// Signup User
 router.post('/create', validateUserDetails, signup);
+
+// Login User
 router.post('/login', validateUserDetails, login);
+
+// Fetch User profile
 router.get('/profile', authUser, getUserProfile);
 
 export default router;
