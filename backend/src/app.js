083d3bc3b1c70b8065dfc09fadc3ba9ef963d137
pg147 <xyzprathamesh147@gmail.com>
2025-04-18 20:52:52 +0530

    update: router added for captain

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-// Routes
+// Routers
 import userRouter from './routes/user.route.js';
+import captainRouter from "./routes/captain.route.js";
 
 dotenv.config(); // initialized dotenv for env variables
 
@@ -15,6 +16,11 @@ app.use(express.json());  // middleware to parse data in json
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());  // middleware to parse cookies
 app.use(cors());  // middleware for cross-origin-resource-sharing
-app.use('/api/v1/users', userRouter);
+
+app.use('/api/v1'); // base route
+
+// Routes
+app.use('/users', userRouter);
+app.use('/captains', captainRouter);
 
 export { app };
