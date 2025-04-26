80cab4b0cc79b3ed1a545176ce5c1e80477f0b49
pg147 <xyzprathamesh147@gmail.com>
2025-04-27 00:14:16 +0530

    update: captain routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import { Home, UserSignup } from "./pages";
-import UserLogin from "./pages/UserLogin.jsx";
+import { Home, UserSignup, UserLogin, CaptainLogin, CaptainSignup } from "./pages";
 
 function App() {
     return (
         <>
             <Routes>
                 <Route path={'/'} element={<Home />} />
+
                 <Route path={'/user/signup'} element={<UserSignup />} />
                 <Route path={'/user/login'} element={<UserLogin />} />
+
+                <Route path={'/captain/login'} element={<CaptainLogin />} />
+                <Route path={'/captain/signup'} element={<CaptainSignup />} />
             </Routes>
         </>
     );
