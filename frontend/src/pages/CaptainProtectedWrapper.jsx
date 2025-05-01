df3986a7d32f24fffa9e6aafdd2f4ca20df81b0a
pg147 <xyzprathamesh147@gmail.com>
2025-05-01 20:12:18 +0530

    update

--- frontend/src/pages/CaptainProtectedWrapper.jsx
+++ frontend/src/pages/CaptainProtectedWrapper.jsx
@@ -1,17 +1,66 @@
-import { useEffect } from "react";
+// React imports
+import React, { useContext, useEffect, useState } from 'react';
+
+// React-Router-DOM
 import { useNavigate } from "react-router-dom";
 
+// Axios
+import { axiosInstance } from "../libs/axios.js";
+
+// Context API
+import { captainDataContext } from "../context/captainContext.jsx";
+
+// Icons Libraries
+import { LuLoaderCircle } from "react-icons/lu";
+
 function CaptainProtectedWrapper({ children }) {
-    const cToken = localStorage.getItem('cToken');
+    const cToken = localStorage.getItem('cToken');  // fetching captain token from localStorage
     const navigate = useNavigate();
+
+    // Loading states
+    const [loading, setLoading] = useState(false);
+
+    const { setCaptain } = useContext(captainDataContext);
+
+    const fetchCaptainProfile = async () => {
+        try {
+            setLoading(true);
+
+            const response = await axiosInstance.get('/captains/profile', {
+                headers: {
+                    authorization: `Bearer ${cToken}`
+                }
+            });
+
+            const responseData = response.data;
+
+            if (responseData && responseData.success) {
+                setCaptain(responseData.data.captain);
+                setLoading(false);
+            }
+        } catch (error) {
+            console.log("Error loading captain : ", error.message);
+            localStorage.removeItem('cToken');
+            setLoading(false);
+            navigate('/captain/login');
+        }
+    }
 
     useEffect(() => {
         if (!cToken) {
             navigate('/captain/login');
-        }
+        } else fetchCaptainProfile();
     }, [cToken]);
 
-    return (
+    if (loading) {
+        return (
+            <div className={'h-screen flex w-full items-center justify-center'}>
+                <div className={'flex items-center space-x-2.5'}>
+                    <LuLoaderCircle className={"size-8 animate-spin text-primary"}/>
+                </div>
+            </div>
+        );
+    } else return (
         <>
             {children}
         </>
--- frontend/src/pages/UserProtectedWrapper.jsx
+++ frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,17 +1,66 @@
-import React, { useEffect } from 'react';
+// React imports
+import React, { useContext, useEffect, useState } from 'react';
+
+// React-Router-DOM
 import { useNavigate } from "react-router-dom";
 
+// Axios
+import { axiosInstance } from "../libs/axios.js";
+
+// Context API
+import { userDataContext } from "../context/userContext.jsx";
+
+// Icons Libraries
+import { LuLoaderCircle } from "react-icons/lu";
+
 function UserProtectedWrapper({ children }) {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token');  // fetching user token from localStorage
     const navigate = useNavigate();
+
+    // Loading states
+    const [loading, setLoading] = useState(false);
+
+    const { setUser } = useContext(userDataContext);
+
+    const fetchUserProfile = async () => {
+        try {
+            setLoading(true);
+
+            const response = await axiosInstance.get('/users/profile', {
+                headers: {
+                    authorization: `Bearer ${token}`
+                }
+            });
+
+            const responseData = response.data;
+
+            if (responseData && responseData.success) {
+                setUser(responseData.data.user);
+                setLoading(false);
+            }
+        } catch (error) {
+            console.log("Error loading user : ", error.message);
+            localStorage.removeItem('token');
+            setLoading(false);
+            navigate('/user/login');
+        }
+    }
 
     useEffect(() => {
         if (!token) {
             navigate('/user/login');
-        }
+        } else fetchUserProfile();
     }, [token]);
 
-    return (
+    if (loading) {
+        return (
+            <div className={'h-screen flex w-full items-center justify-center'}>
+                <div className={'flex items-center space-x-2.5'}>
+                    <LuLoaderCircle className={"size-8 animate-spin text-primary"}/>
+                </div>
+            </div>
+        );
+    } else return (
         <>
             {children}
         </>
